dddafc53df4426b0f4db31df24b5d46119a41a88
kompanycoder <mwegagideon@gmail.com>
2019-10-22 17:22:16 +0300

    backend working sussessfully with new dependencies and setup listening port

--- server-side/index.js
+++ server-side/index.js
@@ -1,3 +1,4 @@
+// dependencies
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -5,7 +6,24 @@ const morgan =require("morgan");
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.listen(()=>{
+// middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//  init home route for testing
+app.get('/',(req,res)=>{
+    res.json({
+        "success": true,
+        "home": "Connected to mini-netflix backend successfully!!!"
+    })
+})
+
+
+
+// init app
+app.listen(port,()=>{
     console.log("App is listening on:" +port);
 })
 module.exports = app;
