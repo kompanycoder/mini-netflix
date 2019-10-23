027d2faf93f2f285936df53c3873e2b64bc2c6f6
kompanycoder <mwegagideon@gmail.com>
2019-10-23 18:43:06 +0300

    added header,movies-layout and footer components to app

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { MoviesLayoutComponent } from './components/layout/movies-layout/movies-layout.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MoviesLayoutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
