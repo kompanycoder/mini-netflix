1d611813892e4ed1ba83c186516adb33926e7282
kompanycoder <mwegagideon@gmail.com>
2019-10-26 21:14:38 +0300

    added rollbar configuration to angular app

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -1,11 +1,37 @@
+import * as Rollbar from 'rollbar';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Injectable,
+  Injector,
+  InjectionToken,
+  NgModule,
+  ErrorHandler } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { MoviesLayoutComponent } from './components/layout/movies-layout/movies-layout.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
+
+const rollbarConfig = {
+  accessToken: '20b080fd08b04ca384fc5c68d8e13cb6',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+};
+
+@Injectable()
+export class RollbarErrorHandler implements ErrorHandler {
+  constructor(@Inject(RollbarService) private rollbar: Rollbar) {}
+
+  handleError(err:any) : void {
+    this.rollbar.error(err.originalError || err);
+  }
+}
+
+export function rollbarFactory() {
+    return new Rollbar(rollbarConfig);
+}
+
+export const RollbarService = new InjectionToken<Rollbar>('rollbar');
 
 @NgModule({
   declarations: [
@@ -18,7 +44,10 @@ import { FooterComponent } from './components/layout/footer/footer.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: ErrorHandler, useClass: RollbarErrorHandler },
+    { provide: RollbarService, useFactory: rollbarFactory }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
