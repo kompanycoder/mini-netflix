f3b4bd180a12ae0f5f29254d2ac7fd09eea6122b
kompanycoder <mwegagideon@gmail.com>
2019-10-26 18:02:29 +0300

    connected mongoose to mongo successfully and played around with numerous inbuilt getters and promises

--- server-side/index.js
+++ server-side/index.js
@@ -4,16 +4,37 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
 // const port = 3000;
+const mongoose = require("mongoose");
 const app = express();
+
+// make global mongoose promise here
+const promise = mongoose.Promise;
 
 // include user and movie models
 const { User } = require("./models/user/user");
 const { Movie } = require("./models/movie/movie");
-console.log(User);
-console.log(Movie);
+// console.log(User);
+// console.log(Movie);
+
 // Include config
 const configOptions = require("./config/config").get(process.env.NODE_ENV);
-// console.log(configOptions);
+console.log(configOptions);
+
+let mongoDbOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
+// let app connect to db here
+mongoose.connect(configOptions.dbUrl, mongoDbOptions, (err, results)=>{
+    if(err) return err;
+    console.log("Connected to NetflixDb on mongo server");
+    // let Db = results.db;
+    console.log(results.db);
+    console.log(results.db.databaseName);
+    // console.log(results.db.serverConfig);
+    // let NetflixDb = Db.getCollection("Netflix");
+    // console.log(NetflixDb);
+})
 
 // middlewares
 app.use(cors());
@@ -35,4 +56,6 @@ app.use(movieRoutes);
 app.listen(configOptions.port, () => {
   console.log("App backend is listening on:" + configOptions.port);
 });
+
+// export app
 module.exports = app;
